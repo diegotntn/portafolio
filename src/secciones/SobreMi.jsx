64d359e1f8b3b7d8f0e5ccce450490a263630412
diegotntn <diego.telnie.2004@gmail.com>
2026-04-20 16:06:50 -0600

    sobre mi cambios diseño

--- src/secciones/SobreMi.jsx
+++ src/secciones/SobreMi.jsx
@@ -6,15 +6,24 @@ import { l_sobre_mi } from "../datos/sobreMi";
 function SobreMi() {
   return (
     <section className="sobre-mi" id="sobre-mi">
+
+      {/* Barra superior */}
+      <div className="sobre-mi__header">
+        <span className="sobre-mi__seccion-label">Sobre mí</span>
+        <span className="sobre-mi__seccion-num">03 / 04</span>
+      </div>
+
       <div className="sobre-mi__contenedor">
 
+        {/* Título */}
         <h2 className="sobre-mi__titulo">
-          SOBRE MÍ
+          Sobre<br />
+          <span className="titulo-invertido">mí</span>
         </h2>
 
+        {/* Tarjetas hover */}
         <div className="sobre-mi__zona-tarjetas">
           {l_sobre_mi.map((tarjeta) => {
-
             const {
               id,
               tituloBoton,
@@ -38,7 +47,6 @@ function SobreMi() {
                 />
               </div>
             );
-
           })}
         </div>
 
